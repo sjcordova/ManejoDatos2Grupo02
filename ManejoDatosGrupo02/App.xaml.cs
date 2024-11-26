78e363edc434c4ffc01355d9b81ac3091d2c20ed
laboratorio <laboratorio@UPOAULA103>
2024-11-26 07:11:47 -0500

    Agregar archivos de proyecto.

---
+++ ManejoDatosGrupo02/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ManejoDatosGrupo02
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ManejoDatosGrupo02/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ManejoDatosGrupo02
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
